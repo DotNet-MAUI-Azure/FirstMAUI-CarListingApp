b01172a67cfc799c6948b1abd480313e0e3c4b62
Auravision <auravision.africa@gmail.com>
2022-11-11 14:37:36 +0300

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AuraCarListingsApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AuraCarListingsApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
